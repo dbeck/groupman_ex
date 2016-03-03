8fbd3230ca85519403b0ac64df4c4107b1c27c4a
David Beck <dbeck@beckground.hu>
2016-03-03 13:38:50 +0100

    fixing mix.exs and configs

--- mix.exs
+++ mix.exs
@@ -4,13 +4,13 @@ defmodule GroupManager.Mixfile do
   def project do
     [
       app: :group_manager,
-      version: "0.0.6",
+      version: "0.0.7",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps,
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock"
+      description: description,
+      package: package,
+      deps: deps
     ]
   end
 
@@ -23,11 +23,25 @@ defmodule GroupManager.Mixfile do
 
   defp deps do
     [
-      {:chatter, "~> 0.0.10"},
-      {:snappy, "~> 1.1"},
       {:xxhash, git: "https://github.com/pierresforge/erlang-xxhash"},
-      {:exactor, "~> 2.2"},
-      {:ranch, "~> 1.2"}
+      {:chatter, "~> 0.0.12"}
     ]
+  end
+
+  defp description do
+    """
+    GroupManager is extracted from the ScaleSmall project as a standalone piece.
+    This can be used independently to manage a set of nodes and coordinate a
+    common goal expressed as integer ranges.
+    """
+  end
+
+  defp package do
+    [
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["David Beck"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/dbeck/groupman_ex",
+              "Docs" => "http://dbeck.github.io/groupman_ex/"}]
   end
 end
